14846b5f68d8e2c96514dfda0aae7f20575ea8f0
jeisacarvalho <jeisasilva1823@gmail.com>
2023-04-30 16:11:15 -0300

    react

--- Projeto-Filmes/src/pages/Home.jsx
+++ Projeto-Filmes/src/pages/Home.jsx
@@ -1,21 +1,26 @@
-import {useState, useEffect} from "react"
+import { useState, useEffect } from "react";
 
 const moviesURL = import.meta.env.VITE_API;
-const apiKey = import.meta.env.VITE_API_KEY;
+const apikey = import.meta.env.VITE_API_KEI;
 
-function Home() {
+const Home = () => {
     const [topMovies, setTopMovies] = useState([]);
 
-    const getTopRatedMovies = async (url) => {
-        const res = await fetch(url);
+    const getTopRatedMovies = async (URL) => {
+
+        const res = await fetch(URL);
         const data = await res.json();
 
         console.log(data);
-    }
-    
-    return (
-        <div>Home</div>
+    };
 
-    )
-}
-export default Home
+    useEffect(() => {
+
+        const topRatedUrl = `${moviesURL}top_rated?${apikey}`;
+
+       getTopRatedMovies(topRatedUrl)
+    }, []);
+    return <div>Home</div>;
+};
+
+export default Home;
